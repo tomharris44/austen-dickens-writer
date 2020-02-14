9105ad467e0d0a24e1184d0884270f9f9b085294
Tom Harris <harristom109@gmail.com>
2020-02-14 14:57:53 +1100

    removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/tomharris44/Documents/misc/django_proj/gpt2_writer/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
